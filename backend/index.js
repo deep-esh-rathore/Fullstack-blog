b76f41e7a65712fdddbb7d6563cc1834b6468a6f
deep-esh-rathore <dheetnew@gmail.com>
2025-09-10 23:34:09 +0530

    fix(cors): allow Vercel frontend domain in CORS config

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(cors(
     {
         origin: ["http://localhost:5173",
-            "https://fullstack-blog-z5u3.onrender.com"
+            "https://fullstack-blog-self.vercel.app"
         ],
 
 
